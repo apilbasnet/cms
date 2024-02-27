c01e10e80811ef503470dcfdc71919242b51291c
skdhg <46562212+skdhg@users.noreply.github.com>
2024-02-27 18:32:30 +0545

    add student form

--- app/dashboard/add-student/page.tsx
+++ app/dashboard/add-student/page.tsx
@@ -1,10 +1,166 @@
+'use client';
+import {
+  Button,
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  Input,
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from '@edge-ui/react';
+import React from 'react';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { useForm } from 'react-hook-form';
+import { z } from 'zod';
+import supabase from '@/lib/client';
 
-import React from 'react'
+const SWASTIK_TLD = '@swastikcollege.edu.np';
+
+const formSchema = z.object({
+  name: z.string().min(2, {
+    message: 'Name must be at least 2 characters.',
+  }),
+  email: z.string().email().endsWith(SWASTIK_TLD, {
+    message: "Email must use Swastik College's domain name.",
+  }),
+  phone: z.string().length(10),
+  address: z.string(),
+  password: z.string().min(8).max(32),
+  batch: z.string().length(4),
+  faculty: z.string(),
+});
 
 const AddStudentPage = () => {
-  return (
-    <div>AddStudentPage</div>
-  )
-}
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {},
+  });
 
-export default AddStudentPage
+  const onSubmit = async (values: z.infer<typeof formSchema>) => {
+    const result = await supabase.auth.admin.createUser({
+      user_metadata: values,
+      email: values.email,
+      email_confirm: false,
+      password: values.password,
+      role: 'student',
+    });
+
+    console.log(result);
+  };
+
+  return (
+    <div className="flex items-center justify-start flex-col gap-4 w-full py-8">
+      <h1 className="font-medium text-xl">Add student</h1>
+      <Form {...form}>
+        <form
+          className="w-1/2 space-y-4"
+          onSubmit={form.handleSubmit(onSubmit)}
+        >
+          <FormField
+            control={form.control}
+            name="name"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel htmlFor="name">Name</FormLabel>
+                <FormControl>
+                  <Input id="name" type="text" {...field} />
+                </FormControl>
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="email"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel htmlFor="email">Email</FormLabel>
+                <FormControl>
+                  <Input id="email" type="email" {...field} />
+                </FormControl>
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="phone"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel htmlFor="phone">Phone</FormLabel>
+                <FormControl>
+                  <Input id="phone" type="tel" {...field} />
+                </FormControl>
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="address"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel htmlFor="address">Address</FormLabel>
+                <FormControl>
+                  <Input id="address" type="text" {...field} />
+                </FormControl>
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="password"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel htmlFor="password">Password</FormLabel>
+                <FormControl>
+                  <Input id="password" type="password" {...field} />
+                </FormControl>
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="batch"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel htmlFor="batch">Batch</FormLabel>
+                <FormControl>
+                  <Input id="batch" type="text" {...field} />
+                </FormControl>
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="faculty"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel htmlFor="faculty">Faculty</FormLabel>
+                <FormControl>
+                  <Select
+                    onValueChange={field.onChange}
+                    defaultValue={field.value}
+                  >
+                    <SelectTrigger className="w-full">
+                      <SelectValue placeholder="Faculty name" />
+                    </SelectTrigger>
+                    <SelectContent>
+                      <SelectItem value="BCA">BCA</SelectItem>
+                      <SelectItem value="CSIT">CSIT</SelectItem>
+                    </SelectContent>
+                  </Select>
+                </FormControl>
+              </FormItem>
+            )}
+          />
+          <Button className="w-full">Create</Button>
+        </form>
+      </Form>
+    </div>
+  );
+};
+
+export default AddStudentPage;
