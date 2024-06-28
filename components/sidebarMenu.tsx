ef6868d5b5862af7b2aa3c38b96e6567594eb87c
twlite <46562212+twlite@users.noreply.github.com>
2024-06-28 09:38:15 +0545

    fix

--- components/sidebarMenu.tsx
+++ components/sidebarMenu.tsx
@@ -1,9 +1,9 @@
-"use client";
-import classNames from "classnames";
-import Link from "next/link";
-import { usePathname } from "next/navigation";
+'use client';
+import classNames from 'classnames';
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
 
-import { useMemo } from "react";
+import { useMemo } from 'react';
 import {
   Home,
   Profile,
@@ -14,86 +14,86 @@ import {
   ManageStaff,
   Notify,
   Attendance,
-} from "./icons/icons";
-import SwastikIcon from "./icons/Swastik.png";
-import Image from "next/image";
-import { useUser } from "@/lib/context/UserContext";
-import { RoleType } from "@/lib/api/user.api";
+} from './icons/icons';
+import SwastikIcon from './icons/Swastik.png';
+import Image from 'next/image';
+import { useUser } from '@/lib/context/UserContext';
+import { RoleType } from '@/lib/api/user.api';
 
 const menuItems = [
   {
-    label: "Dashboard",
-    link: "/dashboard",
+    label: 'Dashboard',
+    link: '/dashboard',
     icon: Home,
     role: [RoleType.ADMIN, RoleType.STUDENT, RoleType.TEACHER],
   },
   {
-    label: "Profile",
-    link: "/dashboard/profile",
+    label: 'Profile',
+    link: '/dashboard/profile',
     icon: Profile,
     role: [RoleType.ADMIN, RoleType.STUDENT, RoleType.TEACHER],
   },
   {
-    label: "Course",
-    link: "/dashboard/course",
+    label: 'Course',
+    link: '/dashboard/course',
     icon: Class,
     role: [RoleType.ADMIN, RoleType.STUDENT, RoleType.TEACHER],
   },
   {
-    label: "Subject",
-    link: "/dashboard/subject",
+    label: 'Subject',
+    link: '/dashboard/subject',
     icon: Subject,
     role: [RoleType.ADMIN, RoleType.STUDENT, RoleType.TEACHER],
   },
 
   {
-    label: "Add Student",
-    link: "/dashboard/add-student",
+    label: 'Add Student',
+    link: '/dashboard/add-student',
     icon: AddStaff,
     role: [RoleType.ADMIN],
   },
   {
-    label: "Manage Student",
-    link: "/dashboard/manage-student",
+    label: 'Manage Student',
+    link: '/dashboard/manage-student',
     icon: ManageStaff,
     role: [RoleType.ADMIN],
   },
   {
-    label: "Add Staff",
-    link: "/dashboard/add-staff",
+    label: 'Add Staff',
+    link: '/dashboard/add-staff',
     icon: AddStaff,
     role: [RoleType.ADMIN],
   },
   {
-    label: "Manage Staff",
-    link: "/dashboard/manage-staff",
+    label: 'Manage Staff',
+    link: '/dashboard/manage-staff',
     icon: ManageStaff,
     role: [RoleType.ADMIN],
   },
 
   {
-    label: "My Notifications",
-    link: "/dashboard/notifications",
+    label: 'My Notifications',
+    link: '/dashboard/notifications',
     icon: Notify,
     role: [RoleType.STUDENT, RoleType.TEACHER],
   },
   {
-    label: "Notify Staff",
-    link: "/dashboard/notify-staff",
+    label: 'Notify Staff',
+    link: '/dashboard/notify-staff',
     icon: Notify,
     role: [RoleType.ADMIN],
   },
   {
-    label: "Notify Student",
-    link: "/dashboard/notify-student",
+    label: 'Notify Student',
+    link: '/dashboard/notify-student',
     icon: Notify,
     role: [RoleType.ADMIN, RoleType.TEACHER],
   },
   {
-    label: "Attendance",
-    link: "/dashboard/attendance",
+    label: 'Attendance',
+    link: '/dashboard/attendance',
     icon: Attendance,
-    role: [RoleType.ADMIN, RoleType.TEACHER],
+    role: [RoleType.TEACHER],
   },
 
   // {
@@ -121,9 +121,9 @@ const SidebarMenu = () => {
 
   const getNavItemClasses = (menu: Partial<(typeof menuItems)[0]>) => {
     return classNames(
-      "flex items-center cursor-pointer hover:bg-muted rounded w-full overflow-hidden whitespace-nowrap",
+      'flex items-center cursor-pointer hover:bg-muted rounded w-full overflow-hidden whitespace-nowrap',
       {
-        ["bg-muted"]: activeMenu && activeMenu.link === menu.link,
+        ['bg-muted']: activeMenu && activeMenu.link === menu.link,
       }
     );
   };
@@ -131,7 +131,7 @@ const SidebarMenu = () => {
   return (
     <div
       className={`h-screen px-4 py-2 bg-light flex justify-between flex-col scroll-smooth w-60 border-r`}
-      style={{ transition: "width 300ms cubic-bezier(0.2, 0, 0, 1) 0s" }}
+      style={{ transition: 'width 300ms cubic-bezier(0.2, 0, 0, 1) 0s' }}
     >
       <div className="flex flex-col  overflow-auto ">
         <div className="flex items-center justify-between relative ">
@@ -148,17 +148,17 @@ const SidebarMenu = () => {
               <div
                 key={menu.label}
                 className={`flex items-center cursor-pointer hover:bg-muted rounded w-full overflow-hidden whitespace-nowrap ${
-                  activeMenu && activeMenu.link === menu.link ? "bg-muted" : ""
+                  activeMenu && activeMenu.link === menu.link ? 'bg-muted' : ''
                 }`}
               >
                 <Link href={menu.link} legacyBehavior>
                   <a className="flex py-3 px-3 items-center w-full h-full ">
-                    <div style={{ width: "2.5rem" }}>
+                    <div style={{ width: '2.5rem' }}>
                       <Icon className="h-5 w-5" />
                     </div>
                     <span
                       className={classNames(
-                        "text-md font-medium text-text-light"
+                        'text-md font-medium text-text-light'
                       )}
                     >
                       {menu.label}
@@ -179,7 +179,7 @@ const SidebarMenu = () => {
           <div className="w-10">
             <Logout />
           </div>
-          <span className={classNames("text-md font-medium text-text-light")}>
+          <span className={classNames('text-md font-medium text-text-light')}>
             Logout
           </span>
         </button>
